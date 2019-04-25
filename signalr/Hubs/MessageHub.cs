a12f607bf7462a541b4479eed0b19ff4422ff463
Ivan Lutsyk <ivanlutsyk87@gmail.com>
2019-04-25 17:58:42 +0300

    first commit

---
+++ signalr/Hubs/MessageHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalr.Hubs
+{
+    public class MessageHub: Hub
+    {
+        
+        public Task SendMessageToAll(string message)
+        {
+            return  Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToCaller(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+        
+        public Task SendMessageToUser(string connectionId, string message)
+        {
+            return Clients.Client(connectionId).SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ signalr/wwwroot/Client/js/message.js
@@ -0,0 +1,60 @@
+﻿"use strict";
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/messages")
+    .build();
+
+connection.on("ReceiveMessage", function (message) {
+    //debugger
+    var msg = message
+        .replace(/!/g, "!amp;")
+        .replace(/</g, "!lt;")
+        .replace(/>/g, "!gt;");
+    var div = document.createElement("div");
+    div.innerHTML = msg + "<hr/>";
+
+    document.getElementById("messages").appendChild(div);
+});
+
+connection.on("UserConnected", function (connectionId) {
+    var groupElement = document.getElementById("group");
+    var option = document.createElement("option");
+    option.text = connectionId;
+    option.value = connectionId;
+    groupElement.add(option);
+});
+
+connection.on("UserDisconnected", function (connectionId) {
+    var groupElement = document.getElementById("group");
+    for (var i = 0; i < groupElement.length; i++) {
+        if (groupElement.options[i] === connectionId) {
+            groupElement.remove(i);
+        }
+    }
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    //debugger
+    var message = document.getElementById("message").value;
+    var groupElement = document.getElementById("group");
+    var groupElementValue = groupElement.options[groupElement.selectedIndex].value;
+    
+    if (groupElementValue === "All" || groupElementValue === "MaySelf") {
+       var method = groupElementValue === "All"
+            ? "SendMessageToAll"
+            : "SendMessageToCaller";
+
+        connection.invoke(method, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    } else {
+        connection.invoke("SendMessageToUser",groupElementValue,message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    
+    event.preventDefault();
+});
